af8238adf78db2864ade455611dd9c0ac34bf945
neverenderneko <brim47702@gmail.com>
2026-08-18 16:23:57 -0500

    Create rpg_char.py

---
+++ rpg_char.py
@@ -0,0 +1,27 @@
+full_dot = '●'
+empty_dot = '○'
+
+def create_character(character_name, STR, INT, CHA):
+    if not isinstance(character_name, str):
+        return 'The character name should be a string'
+    elif character_name == '':
+        return 'The character should have a name'
+    elif len(character_name) > 10:
+        return 'The character name is too long'
+    elif ' ' in character_name:
+        return 'The character name should not contain spaces'
+    elif not isinstance(STR, int) or not isinstance(INT, int) or not isinstance(CHA, int):
+        return 'All stats should be integers'
+    elif STR < 1 or INT < 1 or CHA < 1:
+        return 'All stats should be no less than 1'
+    elif STR > 4 or INT > 4 or CHA > 4:
+        return 'All stats should be no more than 4'
+    elif STR + INT + CHA != 7:
+        return 'The character should start with 7 points'
+    else:
+        return f'''{character_name}
+STR {full_dot * STR + empty_dot * (10 - STR)}
+INT {full_dot * INT + empty_dot * (10 - INT)}
+CHA {full_dot * CHA + empty_dot * (10 - CHA)}'''
+
+print(create_character('ren', 4, 2, 1))
